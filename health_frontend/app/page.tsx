2a3fd538a45c72011cabbbcbdc783de9d67477b5
Peachlambuct <118652252+Peachlambuct@users.noreply.github.com>
2024-09-26 23:40:33 +0800

    测试前端提交

--- health_frontend/app/page.tsx
+++ health_frontend/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your changesss instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
