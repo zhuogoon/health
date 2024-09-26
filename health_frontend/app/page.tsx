796572baefbedc9cb7f74fdd9360b3db3400c6d1
Peachlambuct <118652252+Peachlambuct@users.noreply.github.com>
2024-09-26 23:31:47 +0800

    测试前端提交

--- health_frontend/app/page.tsx
+++ health_frontend/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
           Examples
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          className="flex hhh items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
