dfb773bcada90a662344a75d170343faaafa803b
Peachlambuct <118652252+Peachlambuct@users.noreply.github.com>
2024-10-17 13:38:46 +0800

    病例模块前后联调完成

---
+++ health_backend/models/request/doctorCase.go
@@ -0,0 +1,13 @@
+package response
+
+type DoctorCase struct {
+	Id          uint   `json:"id"`
+	PatientName string `json:"patient_name"`
+	Sex         string `json:"sex"`
+	Age         int    `json:"age"`
+	DoctorType  string `json:"doctor_type"`
+	CheckIDs    string `json:"checkIDs"`
+	Date        string `json:"date"`
+	Title       string `json:"title"`
+	Content     string `json:"content"`
+}
